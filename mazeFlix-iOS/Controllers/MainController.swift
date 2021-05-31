0660cbe5212f1d29169298c43c98e2b52217b1c9
Tiago Marques <ttsmarques@gmail.com>
2021-05-31 18:06:52 -0300

    Create project

--- mazeFlix-iOS/Controllers/MainController.swift
+++ mazeFlix-iOS/Controllers/MainController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainController.swift
 //  mazeFlix-iOS
 //
 //  Created by Tiago Marques on 31/05/21.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
