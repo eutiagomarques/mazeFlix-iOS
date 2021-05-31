d1d00001ed31afaf263baa20a063429a9aa03b44
Tiago Marques <tiago.marques@radixeng.com.br>
2021-05-31 17:25:43 -0300

    Initial Commit

---
+++ mazeFlix-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mazeFlix-iOS
+//
+//  Created by Tiago Marques on 31/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
